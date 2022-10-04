358bfb6775d65a748d2aae020d72ef09f869de10
Pierangelo Di Pilato <pierangelodipilato@gmail.com>
2022-10-04 17:23:14 +0200

    Replace base Go image based on go.mod (#426)
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- openshift/ci-operator/generate-dockerfiles.sh
+++ openshift/ci-operator/generate-dockerfiles.sh
@@ -2,8 +2,13 @@
 
 set -e
 
+# shellcheck disable=SC2002
+go_version=$(grep "^go.*" "go.mod" | awk '{print $2}')
+dockerfile_replace="s|registry.ci.openshift.org/openshift/release:golang-[a-zA-Z0-9.]*|registry.ci.openshift.org/openshift/release:golang-${go_version}|g"
+
 function generate_dockefiles() {
   local target_dir=$1; shift
+  sed -i "${dockerfile_replace}" "openshift/ci-operator/Dockerfile.in"
   # Remove old images and re-generate, avoid stale images hanging around.
   for img in $@; do
     local image_base=$(basename $img)
@@ -13,8 +18,3 @@ function generate_dockefiles() {
 }
 
 generate_dockefiles $@
-
-# shellcheck disable=SC2002
-go_version=$(grep "^go.*" "go.mod" | awk '{print $2}')
-
-sed -i "s|registry.ci.openshift.org/openshift/release:golang.*|registry.ci.openshift.org/openshift/release:golang-${go_version}|g" openshift/ci-operator/build-image/Dockerfile
