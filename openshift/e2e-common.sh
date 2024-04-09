c47d8689402e5c5f9ed8b64b5070d4630904023f
Christoph Stäbler <cstabler@redhat.com>
2024-04-09 16:00:51 +0200

    Add OIDC tests to encryption/auth test suite (#1045)

--- openshift/e2e-common.sh
+++ openshift/e2e-common.sh
@@ -163,7 +163,7 @@ function run_e2e_encryption_auth_tests(){
 
   oc wait --for=condition=Ready knativeeventing.operator.knative.dev knative-eventing -n "${EVENTING_NAMESPACE}" --timeout=900s || return $?
 
-  local regex="TLS"
+  local regex="TLS|OIDC"
 
   local test_name="${1:-}"
   local run_command="-run ${regex}"
@@ -173,7 +173,7 @@ function run_e2e_encryption_auth_tests(){
       local run_command="-run ^(${test_name})$"
   fi
   # check for test flags
-  RUN_FLAGS="-timeout=1h -run ${regex}"
+  RUN_FLAGS="-timeout=1h ${run_command}"
   go_test_e2e ${RUN_FLAGS} ./test/e2e_new --images.producer.file="${images_file}" || failed=$?
 
   return $failed
